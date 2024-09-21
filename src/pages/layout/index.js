e8d831d128635c33279fffc51890a7cb180ddbca
Yat Siu Tseung <yatsiutseung@gmail.com>
2024-09-21 17:39:14 +0100

    implemented the mouthly calculation

--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -1,20 +1,61 @@
-import { Outlet } from "react-router-dom"
-import { Button } from "antd-mobile"
-import { useEffect } from "react"
-import { UseDispatch, useDispatch } from "react-redux"
-import { getBillList } from "../../store/modules/billStore"
+import { TabBar } from 'antd-mobile'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import {
+    BillOutline,
+    CalculatorOutline,
+    AddCircleOutline
+} from 'antd-mobile-icons'
+import './index.scss'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { getBillList } from '../../store/modules/billStore'
+
+const tabs = [
+    {
+        key: '/',
+        title: 'Month Bill',
+        icon: <BillOutline />,
+    },
+    {
+        key: '/new',
+        title: 'Add',
+        icon: <AddCircleOutline />,
+    },
+    {
+        key: '/year',
+        title: 'Year Bill',
+        icon: <CalculatorOutline />,
+    },
+]
 
 const Layout = () => {
     const dispatch = useDispatch()
+
     useEffect(() => {
         dispatch(getBillList())
     }, [dispatch])
-    return (
-        <div>
-            <Outlet />
-            layout
-            <Button color="primary">test</Button>
 
+    const navigate = useNavigate()
+
+    //switch the page when trigger the TabBar
+    const switchRoute = (path) => {
+        console.log(path)
+        navigate(path)
+    }
+
+    return (
+        <div className="layout">
+            <div className="container">
+                <Outlet />
+            </div>
+
+            <div className="footer">
+                <TabBar onChange={switchRoute}>
+                    {tabs.map(item => (
+                        <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+                    ))}
+                </TabBar>
+            </div>
         </div>
     )
 }
